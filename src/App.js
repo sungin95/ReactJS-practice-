82ed79b786ae32469e94df0fa2d4c62565dc0d24
Sungin Lee <dlrkehrud@naver.com>
2023-03-23 14:15:02 +0900

    6.4 useState 생성, 파괴, - function == =>

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('항상 돌아가는 것');
+function Hello() {
   useEffect(() => {
-    console.log('한번만 돌아감');
+    console.log('created:)');
+    return () => console.log('destroyed :(');
   }, []);
-  useEffect(() => {
-    if (keyword !== '' && keyword.length > 5) {
-      console.log('I run when keyword Changes.', keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log('I run when counter Changes.', counter);
-  }, [counter]);
-  useEffect(() => {
-    console.log('I run when counter,keyword Changes.', counter, keyword);
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'show'} </button>
     </div>
   );
 }
