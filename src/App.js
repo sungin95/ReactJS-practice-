60858fadc32b2fe97dcbed4b8e434afacd9858fe
Sungin Lee <dlrkehrud@naver.com>
2023-03-23 18:46:24 +0900

    7.2 code chellenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState(1);
+  const onChange = (event) => {
+    if (event.target.value >= 1) {
+      setMyMoney(event.target.value);
+    }
+  };
   useEffect(() => {
     fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json())
@@ -13,12 +19,22 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? <strong>Loading...</strong> : null}
+
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+      <label htmlFor="money">My money $</label>
+      <input
+        id="money"
+        type="number"
+        value={myMoney}
+        onChange={onChange}
+        placeholder="More than $1"
+      />
+      <br />
       <select>
         {coins.map((coin, index) => (
-          <option key={index} >
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD{' '}
+          <option key={index}>
+            {coin.name} ({coin.symbol}): ${myMoney / coin.quotes.USD.price} USD{' '}
           </option>
         ))}
       </select>
