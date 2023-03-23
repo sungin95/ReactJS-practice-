588c6b7f9a1ea9ad44853bce398c79c4087bcf8d
Sungin Lee <dlrkehrud@naver.com>
2023-03-23 12:13:26 +0900

    6,2 useEffect 작동 시기 조절하기({},[])

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => {
-    setValue((prev) => prev + 1);
-  };
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log('항상 돌아가는 것');
-  const iRunOnlyOnce = () => {
-    console.log('한번만 돌아감')
-  };
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log('한번만 돌아감');
+  }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('I run when keyword Changes.', keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when counter Changes.', counter);
+  }, [counter]);
+  useEffect(() => {
+    console.log('I run when counter,keyword Changes.', counter, keyword);
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
