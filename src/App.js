82e388a383b2ff34d360f8c5d5a3bdb1c814b490
Sungin Lee <dlrkehrud@naver.com>
2023-03-23 11:16:19 +0900

    6.1 useEffect(한번만 실행하기)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  console.log('항상 돌아가는 것');
+  const iRunOnlyOnce = () => {
+    console.log('한번만 돌아감')
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
