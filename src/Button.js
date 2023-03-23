3b3bf089911917a332d9be122887bfde348d33c1
Sungin Lee <dlrkehrud@naver.com>
2023-03-23 10:37:59 +0900

    6.0 특정 변수를 한번만 실행 시키고 싶을 때

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
 function Button({ text }) {
-  return <button className={styles.btn} >{text}</button>;
+  return <button className={styles.btn}>{text}</button>;
 }
 
 Button.propTypes = {
