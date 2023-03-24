bc65e5914087be255d7db58d88e1b1605401e579
Sungin Lee <dlrkehrud@naver.com>
2023-03-24 21:30:24 +0900

    7.3 Movie app 20개 보여주기

--- src/App.js
+++ src/App.js
@@ -2,44 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState(1);
-  const onChange = (event) => {
-    if (event.target.value >= 1) {
-      setMyMoney(event.target.value);
-    }
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      {loading ? <strong>Loading...</strong> : null}
-
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      <label htmlFor="money">My money $</label>
-      <input
-        id="money"
-        type="number"
-        value={myMoney}
-        onChange={onChange}
-        placeholder="More than $1"
-      />
-      <br />
-      <select>
-        {coins.map((coin, index) => (
-          <option key={index}>
-            {coin.name} ({coin.symbol}): ${myMoney / coin.quotes.USD.price} USD{' '}
-          </option>
-        ))}
-      </select>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {' '}
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
