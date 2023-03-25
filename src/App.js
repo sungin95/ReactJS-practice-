fc0a23147f472664281cca7ab7b710f05f851af2
Sungin Lee <dlrkehrud@naver.com>
2023-03-25 09:45:10 +0900

    7.6 Parameters, error:{ id }

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -26,6 +26,7 @@ function Home() {
           {movies.map((movie) => (
             <Movie
               key={movie.ids}
+              id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
