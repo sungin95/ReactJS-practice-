5436433c19c6c828563980e7ee97661dbb438770
Sungin Lee <dlrkehrud@naver.com>
2023-03-25 12:52:26 +0900

    7.7 Publishing, git pages 업로드하기

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Home from './routes/Home';
 import Detail from './routes/Detail';
 function App() {
   return (
-    <Router>
+    <Router >
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Routes>
     </Router>
   );
